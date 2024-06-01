df77ea74498cd433650e183333bfc7f485192ac7
SandipKurmi <sandipkurmi0@gmail.com>
2024-06-01 12:01:03 +0530

    setup express server

--- server.ts
+++ server.ts
@@ -1,5 +1,11 @@
-console.log("test");
+import app from "./src/app";
 
-function test() {
-  console.log("test");
-}
+const startServer = () => {
+  const port = process.env.PORT || 3000;
+
+  app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+  });
+};
+
+startServer();

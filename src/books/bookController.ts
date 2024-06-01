78fc76f668fe3f829f3edcb12e959bec67047aae
SandipKurmi <sandipkurmi0@gmail.com>
2024-06-01 18:16:05 +0530

    setup multer

--- src/books/bookController.ts
+++ src/books/bookController.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
 
 const createBook = async (req: Request, res: Response, next: NextFunction) => {
+  const {} = req.body;
   res.send("Hello World");
 };
 
